dea729bdba7adc67a4daa4f37dbc609ba700abbe
= <=>
2025-01-14 14:11:11 +0100

    Features section added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const menu = document.querySelector(".navbar__menu");
 const burger = document.querySelector(".burger");
+const nav = document.querySelector(".navbar");
 burger.addEventListener("click", function () {
   menu.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    nav.classList.add("navbar--scroll");
+  } else {
+    nav.classList.remove("navbar--scroll");
+  }
 });
